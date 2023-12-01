7a39b9f847b5753771112fc97b4d7d51ed24ba20
Edouard <edouard.legoupil@gmail.com>
2023-12-01 12:11:56 -0500

    more info on deployment

--- inst/extdata/dev/01_dev.R
+++ inst/extdata/dev/01_dev.R
@@ -47,16 +47,18 @@ golem::run_dev()
 
 ## Now start adding modules from console 
 # Modules are like the pipe between your back-office functions and your user
-# Name of the module - "my_first_module"
+# Name of the module - "my_first_module" - use a meaningful name for your module
 # graveler::level_up(name = "my_first_module") 
 
 # In case you added new libraries to be used within your app, get them registered in your namespace
 # attachment::att_amend_desc()
 
 ### Deploy the app ----------
+## Push to github - and then re-install your package from github
 
-## Add manifest for CI/CD 
+## Now Add manifest for CI/CD 
 rsconnect::writeManifest()
 ## For deployment use the app.R file at the root of your project and then use 
 # the deployment button in blue to publish the project http://rstudio.unhcr.org 
+# alternatively you can also deploy as New Content from Git Repository  directly from Posit Connect
 
